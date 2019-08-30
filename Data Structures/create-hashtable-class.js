9b6ae92411cd84f55f64e30cb5e8a4ad34c518ef
Ehsan Rashid <ehsan.rashid@gmail.com>
2019-08-30 13:05:10 +0500

    PriorityQueue & HashTable added

---
+++ Data Structures/create-hashtable-class.js
@@ -0,0 +1,89 @@
+let called = 0;
+let hash = (string) =>
+{
+	++called;
+	let hash = 0;
+	for (let i = 0; i < string.length; ++i)
+	{
+		hash += string.charCodeAt(i);
+	}
+	return hash;
+};
+
+/*
+//let HashTable = function()
+function HashTable()
+{
+	this.collection = {};
+
+	this.add = function(key, value)
+	{
+		let keyHash = hash(String(key));
+		if (!this.collection[keyHash])
+		{
+			this.collection[keyHash] = {};
+		}
+		this.collection[keyHash][key] = value;
+	}
+	
+	this.remove = function(key)
+	{
+		let keyHash = hash(String(key));
+		if (this.collection[keyHash])
+		{
+			for (let k of Object.keys(this.collection[keyHash]))
+			{
+				if (k === key)
+				{
+					delete this.collection[keyHash][key];
+				}
+			}
+		}
+	}
+	
+	this.lookup = function(key)
+	{
+		let value = this.collection[hash(String(key))][key];
+		return value ? value : null;
+	}
+};
+*/
+
+class HashTable
+{
+	constructor()
+	{
+		this.collection = {};
+	}
+	
+	add(key, value)
+	{
+		let keyHash = hash(String(key));
+		if (!this.collection[keyHash])
+		{
+			this.collection[keyHash] = {};
+		}
+		this.collection[keyHash][key] = value;
+	}
+
+	remove(key)
+	{
+		let keyHash = hash(String(key));
+		if (this.collection[keyHash])
+		{
+			for (let k of Object.keys(this.collection[keyHash]))
+			{
+				if (k === key)
+				{
+					delete this.collection[keyHash][key];
+				}
+			}
+		}
+	}
+
+	lookup(key)
+	{
+		let value = this.collection[hash(String(key))][key];
+		return value ? value : null;
+	}
+};
---
+++ Data Structures/create-priority-queue-class.js
@@ -0,0 +1,87 @@
+/*
+//let PriorityQueue = function()
+function PriorityQueue()
+{
+	this.collection = [];
+	
+	this.enqueue = function(item)
+	{
+		let index = this.collection.findIndex(x => x[1] > item[1]);
+		if (index !== -1)
+		{
+			this.collection.splice(index, 0, item);
+		}
+		else
+		{
+			this.collection.push(item);
+		}
+		return this;
+	}
+
+	this.dequeue = function()
+	{
+		// return the Item without the priority number;
+		return this.collection.shift()[0];
+	}
+
+	this.size = function()
+	{
+		return this.collection.length;
+	}
+
+	this.isEmpty = function()
+	{
+		return this.collection.length === 0;
+	}
+	
+	this.print = function()
+	{
+		console.log(this.collection);
+	}
+};
+*/
+
+class PriorityQueue
+{
+	constructor()
+	{
+		this.collection = [];
+	}
+	
+	enqueue(item)
+	{
+		let index = this.collection.findIndex(x => x[1] > item[1]);
+		if (index !== -1)
+		{
+			this.collection.splice(index, 0, item);
+		}
+		else
+		{
+			this.collection.push(item);
+		}
+		return this;
+	}
+	
+	dequeue()
+	{
+		// return the Item without the priority number;
+		return this.collection.shift()[0];
+	}
+
+	size()
+	{
+		return this.collection.length;
+	}
+
+	isEmpty()
+	{
+		return this.collection.length === 0;
+	}
+
+	print()
+	{
+		console.log(this.collection);
+	}
+};
+
+//export default PriorityQueue;
